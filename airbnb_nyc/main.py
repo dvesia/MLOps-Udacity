162cac6ce288b122d02a95fbc5df5b84add31426
dvesia <domenico.vesia@gmail.com>
2022-07-16 07:03:09 +0200

    add check component

--- airbnb_nyc/main.py
+++ airbnb_nyc/main.py
@@ -40,7 +40,7 @@ def go(config: DictConfig):
             # Download file and load in W&B
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
-                entry_point="main",
+                "main",
                 version="main",
                 parameters={
                     "sample": config["etl"]["sample"],
@@ -51,18 +51,36 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "input_artifact": "domenico-vesia/nyc_airbnb/sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean sample",
+                    "output_description": "Data without outliers and null values",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
+
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "domenico-vesia/nyc_airbnb/clean_sample.csv:latest",
+                    "ref": "domenico-vesia/nyc_airbnb/clean_sample.csv:reference",
+                    "kl_threshold": config['data_check']['kl_threshold'],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
-        if "data_split" in active_steps:
+
+
+        if "data_split" in active_steps: 
             ##################
             # Implement here #
             ##################
